646cce70d663ac7a79071b90dd1c03f29eb9f26b
quiye <zamasu@gmail.com>
2016-06-06 01:37:05 +0900

    delete_files

--- 5-bigger_than_resof_3/do2big.sh
+++
@@ -1,3 +0,0 @@
-make clean > memo/logofclean.txt && make > memo/logofmake.txt && (./fileinput_gkl 10 s < sparse_data.txt) > big10-10.txt
-make clean > memo/logofclean.txt && make > memo/logofmake.txt && (./fileinput_gkl 20 s < sparse_data.txt) > big10-20.txt
-make clean > memo/logofclean.txt && make > memo/logofmake.txt && (./fileinput_gkl 30 s < sparse_data.txt) > big10-30.txt
