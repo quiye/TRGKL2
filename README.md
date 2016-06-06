2fc70645e6fcb44f87e9f1069d588de5b8689a28
quiye <zamasu@gmail.com>
2016-06-07 00:33:28 +0900

    modified for SystemD

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 #how2use_try  
 
 gcc -o sparse_nonsymm sparse_nonsymm.c  
-./sparse_nonsymm 10000 1000 20 > sparse_data.txt  
+./sparse_nonsymm 10000 1000 20 sparse_data.txt  
 make clean  
 make  
 \#下の引数は、得る特異対, s(sparse) or d(dense),accuracy = 10^(-x)  
 ./fileinput_gkl 30 s 7 < sparse_data.txt
+
+\#SystemDの時は
+make -f Makefile_4_SystemD
+\#する。
--- sparse_nonsymm.c
+++ sparse_nonsymm.c
@@ -2,20 +2,23 @@
 #include<stdlib.h>
 int main(int argc,char *argv[]){
   int i, j, k, l, m, n, w;
+  FILE *fp;
+  fp = fopen(argv[4],"w");
   m=atoi(argv[1]);
   n=atoi(argv[2]);
   w=atoi(argv[3]);
   srand(1);
   double a;
-  printf("%d\n",m);
-  printf("%d\n",n);
-  printf("%d\n",w*m);
+  fprintf(fp,"%d\n",m);
+  fprintf(fp,"%d\n",n);
+  fprintf(fp,"%d\n",w*m);
   for(i=0;i<m;i++){
     for(j=0;j<w;j++){
       k=rand() % n;
       a=rand()/(double)RAND_MAX;
-      printf("%d %d %30.20f\n",i,k,a);
+      fprintf(fp,"%d %d %30.20f\n",i,k,a);
     }
   }
+  fclose(fp);
   return 0;
 }
