b9630cfed3b0b6c4029913bc9630fc7d53100fb1
quiye <zamasu@gmail.com>
2016-07-20 16:27:40 +0900

    delete EXTERNALs

--- src/DGEBRDG_4_BISIDE.f90
+++ src/DGEBRDG_4_BISIDE.f90
@@ -1,7 +1,7 @@
 SUBROUTINE DGEBRDG_4_BISIDE(N,A,LDA,Q,P)
   IMPLICIT NONE
   INTEGER N,LDA,i,j
-  DOUBLE PRECISION  ZERO,cs,sn
+  DOUBLE PRECISION ZERO,cs,sn
   PARAMETER (ZERO=0.0D+0 )
   DOUBLE PRECISION A(LDA,*),Q(LDA,*),P(LDA,*)
 
@@ -12,21 +12,21 @@ SUBROUTINE DGEBRDG_4_BISIDE(N,A,LDA,Q,P)
   DO i = 1, N-2
      CALL DLARTG( a(i+1,N), a(i,N), CS, SN,a(i+1,N))
      a(i,N)=0
-     IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
+     IF (( cs .NE. 1) .OR. (sn .NE. 0) ) THEN
         CALL DROT (2,A(i+1,i),LDA,A(i,i),LDA,CS,SN)
         CALL DROT (LDA,Q(1,i+1),1,Q(1,i),1,CS,SN)
      END IF
-     IF ( i .NE. 1 ) THEN
+     IF ( i .GT. 1 ) THEN
         DO j=i,2,-1
            CALL DLARTG( a(j+1,j+1),a(j+1,j),CS, SN,a(j+1,j+1))
            a(j+1,j)=0
-           IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
+           IF (( cs .NE. 1) .OR. (sn .NE. 0) ) THEN
               CALL DROT (2,A(j-1,j+1),1,A(j-1,j),1,CS,SN)
               CALL DROT (LDA,P(j+1,1),LDA,P(j,1),LDA,CS,SN)
            END IF
            CALL DLARTG( a(j,j+1), a(j-1,j+1), CS, SN,a(j,j+1))
            a(j-1,j+1)=0
-           IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
+           IF (( cs .NE. 1) .OR. (sn .NE. 0) ) THEN
               CALL DROT (2,a(j,j-1),LDA,a(j-1,j-1),LDA,CS,SN)
               CALL DROT (LDA,Q(1,j),1,Q(1,j-1),1,CS,SN)
            END IF
@@ -34,7 +34,7 @@ SUBROUTINE DGEBRDG_4_BISIDE(N,A,LDA,Q,P)
      END IF
      CALL DLARTG( a(2,2),a(2,1),CS, SN,a(2,2))
      a(2,1)=0
-     IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
+     IF (( cs .NE. 1) .OR. (sn .NE. 0) ) THEN
         CALL DROT (1,A(1,2),1,A(1,1),1,CS,SN)
         CALL DROT (LDA,P(2,1),LDA,P(1,1),LDA,CS,SN)
      END IF
