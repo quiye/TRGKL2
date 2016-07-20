6879c238ef9dce87936c9e1ea26c4e5532b4f9e0
quiye <zamasu@gmail.com>
2016-07-20 15:57:49 +0900

    remove_external,dlartg5

--- src/DGEBRDG_4_BISIDE.f90
+++ src/DGEBRDG_4_BISIDE.f90
@@ -4,14 +4,13 @@ SUBROUTINE DGEBRDG_4_BISIDE(N,A,LDA,Q,P)
   DOUBLE PRECISION  ZERO,cs,sn
   PARAMETER (ZERO=0.0D+0 )
   DOUBLE PRECISION A(LDA,*),Q(LDA,*),P(LDA,*)
-  EXTERNAL DLARTG5,DROT
 
   IF ( n .LE. 2 ) THEN
      RETURN
   END IF
 
   DO i = 1, N-2
-     CALL DLARTG5( a(i+1,N), a(i,N), CS, SN,a(i+1,N))
+     CALL DLARTG( a(i+1,N), a(i,N), CS, SN,a(i+1,N))
      a(i,N)=0
      IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
         CALL DROT (2,A(i+1,i),LDA,A(i,i),LDA,CS,SN)
@@ -19,13 +18,13 @@ SUBROUTINE DGEBRDG_4_BISIDE(N,A,LDA,Q,P)
      END IF
      IF ( i .NE. 1 ) THEN
         DO j=i,2,-1
-           CALL DLARTG5( a(j+1,j+1),a(j+1,j),CS, SN,a(j+1,j+1))
+           CALL DLARTG( a(j+1,j+1),a(j+1,j),CS, SN,a(j+1,j+1))
            a(j+1,j)=0
            IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
               CALL DROT (2,A(j-1,j+1),1,A(j-1,j),1,CS,SN)
               CALL DROT (LDA,P(j+1,1),LDA,P(j,1),LDA,CS,SN)
            END IF
-           CALL DLARTG5( a(j,j+1), a(j-1,j+1), CS, SN,a(j,j+1))
+           CALL DLARTG( a(j,j+1), a(j-1,j+1), CS, SN,a(j,j+1))
            a(j-1,j+1)=0
            IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
               CALL DROT (2,a(j,j-1),LDA,a(j-1,j-1),LDA,CS,SN)
@@ -33,7 +32,7 @@ SUBROUTINE DGEBRDG_4_BISIDE(N,A,LDA,Q,P)
            END IF
         END DO
      END IF
-     CALL DLARTG5( a(2,2),a(2,1),CS, SN,a(2,2))
+     CALL DLARTG( a(2,2),a(2,1),CS, SN,a(2,2))
      a(2,1)=0
      IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
         CALL DROT (1,A(1,2),1,A(1,1),1,CS,SN)
--- src/DGEBRDG_LP1.f90
+++ src/DGEBRDG_LP1.f90
@@ -4,39 +4,24 @@ SUBROUTINE DGEBRDG_LP1(N,A,LDA,Q,P)
   DOUBLE PRECISION  ONE, ZERO,cs,sn
   PARAMETER ( ONE = 1.0D+0, ZERO=0.0D+0 )
   DOUBLE PRECISION A(LDA,*),Q(LDA,*),P(LDA,*)
-  EXTERNAL dlartg,drot
 
   IF ( n .LE. 2 ) THEN
      RETURN
   END IF
   DO i = N, 3, -1
      DO j = 1, i -2
-        ! WRITE(*,*) "i=",i,"j=",j,"step1"
-        ! DO k = 1, n
-        !    WRITE (*,*) A(k,1:n)
-        ! END DO
         CALL DLARTG( a(j+1,i), a(j,i), CS, SN,a(j+1,i))
         a(j,i)=0
         IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
            CALL drot (i-j+1-1,A(j+1,j),LDA,A(j,j),LDA,CS,SN)
            CALL drot (LDA,Q(1,j+1),1,Q(1,j),1,CS,SN)
         END IF
-        ! WRITE(*,*) "step2"
-        ! DO k = 1, n
-        !    WRITE (*,*) A(k,1:n)
-        ! END DO
         CALL DLARTG( a(j+1,j+1),a(j+1,j),CS, SN,a(j+1,j+1))
         a(j+1,j)=0
         IF (( cs .NE. 1) .OR. (sn .NE. 0)  ) THEN
            CALL drot (j+1-1,A(1,j+1),1,A(1,j),1,CS,SN)
            CALL drot (LDA,P(j+1,1),LDA,P(j,1),LDA,CS,SN)
         END IF
-
-        ! WRITE(*,*) "step3"
-        ! DO k = 1, n
-        !    WRITE (*,*) A(k,1:n)
-        ! END DO
-        ! WRITE(*,*)
      END DO
   END DO
   RETURN
