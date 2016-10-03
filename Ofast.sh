a3f7e292c72ede85311efb8be8a0419ffa1e26f3
quiye <zamasu@gmail.com>
2016-10-03 22:30:07 +0900

    up

--- Ofast.sh
+++
@@ -1,7 +0,0 @@
- ./fileinput_gkl_O3 10 s 100 < dd1000000_100000_100.txt    >    res_O3_10_1000000_100000_100.txt
- ./fileinput_gkl_Ofast 10 s 100 < dd1000000_100000_100.txt > res_Ofast_10_1000000_100000_100.txt
- ./fileinput_gkl_O3 20 s 100 < dd1000000_100000_100.txt    >    res_O3_20_1000000_100000_100.txt
- ./fileinput_gkl_Ofast 20 s 100 < dd1000000_100000_100.txt > res_Ofast_20_1000000_100000_100.txt
- ./fileinput_gkl_O3 30 s 100 < dd1000000_100000_100.txt    >    res_O3_30_1000000_100000_100.txt
- ./fileinput_gkl_Ofast 30 s 100 < dd1000000_100000_100.txt > res_Ofast_30_1000000_100000_100.txt
-
